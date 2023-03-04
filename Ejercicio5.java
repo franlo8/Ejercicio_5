83e23a1dfc5f4ce43569ac84b5076da6536006fc
franlo8 <102101111+franlo8@users.noreply.github.com>
2023-03-04 16:36:40 -0500

    Update Ejercicio5.java

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,11 +1,16 @@
 
-class Ejercicio12 {
+import java.util.Scanner;
+
+class Ejercicio5 {
     public static void main(String[] args) {
         double y, x, suma;
+        Scanner leerporteclado = new Scanner(System.in);
         suma = 0;
-        x = 20;
+        System.out.println("Ingrese el valor de x " );
+        x = leerporteclado.nextDouble();
         suma = suma + x;
-        y = 40;
+        System.out.println("Ingrese el valor de y " );
+        y = leerporteclado.nextDouble();
         x = x + Math.pow(y,2);
         suma = suma + (x/y);
         System.out.println("El valor de la suma es " + suma);
